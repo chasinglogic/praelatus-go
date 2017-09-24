aef50500eef8cf19a9acdbbfc181761a1c22773c
Mathew Robinson <chasinglogic@gmail.com>
2017-09-24 17:21:35 -0400

    Remove auto generated hello tests on client

--- client/test/e2e/specs/test.js
+++ client/test/e2e/specs/test.js
@@ -9,11 +9,7 @@ module.exports = {
     const devServer = browser.globals.devServerURL
 
     browser
-      .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
+          .url(devServer)
+          .end()
   }
 }
