e968298079993c3b312540c76a0657d18ba1e2b2
Mathew Robinson <chasinglogic@gmail.com>
2017-10-08 20:07:52 -0400

    Refactor to make Godoc nicer to use

---
+++ cmd/praelatus/main.go
@@ -0,0 +1,137 @@
+// Copyright 2017 Mathew Robinson <mrobinson@praelatus.io>. All rights reserved.
+// Use of this source code is governed by the AGPLv3 license that can be found in
+// the LICENSE file.
+
+package main
+
+import (
+	"os"
+	"strings"
+
+	"github.com/praelatus/praelatus/commands"
+	"github.com/urfave/cli"
+)
+
+var version = "master"
+var commit = ""
+var date = ""
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "praelatus"
+	app.Usage = "Praelatus, an open source bug tracker / ticketing system"
+
+	if !strings.HasPrefix(version, "SNAPSHOT") {
+		app.Version = version
+	} else {
+		app.Version = "DEV-" + commit
+	}
+
+	app.Action = commands.RunServer
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "devmode",
+			Usage: "runs server in devmode which changes some security behavior to ease development",
+		},
+		cli.BoolFlag{
+			Name:  "profile",
+			Usage: "runs server profiling for debugging hotspots in the code",
+		},
+	}
+
+	app.Authors = []cli.Author{
+		{
+			Name:  "Mathew Robinson",
+			Email: "mrobinson@praelatus.io",
+		},
+		{
+			Name:  "Wes Swett",
+			Email: "wswett@praelatus.io",
+		},
+		{
+			Name:  "Ryan Brzezinski",
+			Email: "ryan.brzezinski867@gmail.com",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "seeddb",
+			Usage:  "seed the database with test data",
+			Action: commands.SeedDB,
+		},
+		{
+			Name:   "serve",
+			Usage:  "start running the REST api",
+			Action: commands.RunServer,
+		},
+		{
+			Name:  "config",
+			Usage: "various commands for interacting with praelatus config",
+			Subcommands: []cli.Command{
+				{
+					Name:   "show",
+					Usage:  "view the configuration for this instance, useful for debugging",
+					Action: commands.ShowConfig,
+				},
+				{
+					Name:   "gen",
+					Usage:  "generate a config.json based on the current environment variables or defaults",
+					Action: commands.GenConfig,
+				},
+			},
+		},
+		{
+			Name:   "testdb",
+			Usage:  "will test the connections to the databases",
+			Action: commands.TestDB,
+		},
+		{
+			Name:   "cleandb",
+			Usage:  "will clean the database (remove all data), useful for testing",
+			Action: commands.CleanDB,
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "yes",
+					Usage: "skip the warning prompt",
+				},
+			},
+		},
+		{
+			Name:   "setupdb",
+			Usage:  "Sets up indexes and other administrative tasks for the database",
+			Action: commands.SetupDB,
+		},
+		{
+			Name:  "admin",
+			Usage: "various admin functions for the instance",
+			Subcommands: []cli.Command{
+				{
+					Name:   "createUser",
+					Usage:  "create a user, useful for creating admin accounts",
+					Action: commands.AdminCreateUser,
+					Flags: []cli.Flag{
+						cli.StringFlag{
+							Name: "username",
+						},
+						cli.StringFlag{
+							Name: "password",
+						},
+						cli.StringFlag{
+							Name: "fullName",
+						},
+						cli.StringFlag{
+							Name: "email",
+						},
+						cli.BoolFlag{
+							Name:  "admin",
+							Usage: "Indicates whether the user is a system admin",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
--- praelatus.go
+++ praelatus.go
@@ -1,12 +1,9 @@
-// Copyright 2017 Mathew Robinson <mrobinson@praelatus.io>. All rights reserved.
-// Use of this source code is governed by the AGPLv3 license that can be found in
-// the LICENSE file.
-
 // Praelatus is an Open Source bug tracking and ticketing system. The
 // backend API is written in Go and the frontend is written in Vue.js.
 // You are viewing the Godoc for the Backend if you would like
 // information about how to use the API as a client or how to start
 // working on the backend visit http://docs.praelatus.io
+//
 // Copyright (C) 2017 Mathew Robinson <mrobinson@praelatus.io>
 //
 // This program is free software: you can redistribute it and/or modify
@@ -21,136 +18,6 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-package main
+package praelatus
 
-import (
-	"os"
-	"strings"
-
-	"github.com/praelatus/praelatus/commands"
-	"github.com/urfave/cli"
-)
-
-var version = "master"
-var commit = ""
-var date = ""
-
-func main() {
-	app := cli.NewApp()
-	app.Name = "praelatus"
-	app.Usage = "Praelatus, an open source bug tracker / ticketing system"
-
-	if !strings.HasPrefix(version, "SNAPSHOT") {
-		app.Version = version
-	} else {
-		app.Version = "DEV-" + commit
-	}
-
-	app.Action = commands.RunServer
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "devmode",
-			Usage: "runs server in devmode which changes some security behavior to ease development",
-		},
-		cli.BoolFlag{
-			Name:  "profile",
-			Usage: "runs server profiling for debugging hotspots in the code",
-		},
-	}
-
-	app.Authors = []cli.Author{
-		{
-			Name:  "Mathew Robinson",
-			Email: "mrobinson@praelatus.io",
-		},
-		{
-			Name:  "Wes Swett",
-			Email: "wswett@praelatus.io",
-		},
-		{
-			Name:  "Ryan Brzezinski",
-			Email: "ryan.brzezinski867@gmail.com",
-		},
-	}
-
-	app.Commands = []cli.Command{
-		{
-			Name:   "seeddb",
-			Usage:  "seed the database with test data",
-			Action: commands.SeedDB,
-		},
-		{
-			Name:   "serve",
-			Usage:  "start running the REST api",
-			Action: commands.RunServer,
-		},
-		{
-			Name:  "config",
-			Usage: "various commands for interacting with praelatus config",
-			Subcommands: []cli.Command{
-				{
-					Name:   "show",
-					Usage:  "view the configuration for this instance, useful for debugging",
-					Action: commands.ShowConfig,
-				},
-				{
-					Name:   "gen",
-					Usage:  "generate a config.json based on the current environment variables or defaults",
-					Action: commands.GenConfig,
-				},
-			},
-		},
-		{
-			Name:   "testdb",
-			Usage:  "will test the connections to the databases",
-			Action: commands.TestDB,
-		},
-		{
-			Name:   "cleandb",
-			Usage:  "will clean the database (remove all data), useful for testing",
-			Action: commands.CleanDB,
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "yes",
-					Usage: "skip the warning prompt",
-				},
-			},
-		},
-		{
-			Name:   "setupdb",
-			Usage:  "Sets up indexes and other administrative tasks for the database",
-			Action: commands.SetupDB,
-		},
-		{
-			Name:  "admin",
-			Usage: "various admin functions for the instance",
-			Subcommands: []cli.Command{
-				{
-					Name:   "createUser",
-					Usage:  "create a user, useful for creating admin accounts",
-					Action: commands.AdminCreateUser,
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name: "username",
-						},
-						cli.StringFlag{
-							Name: "password",
-						},
-						cli.StringFlag{
-							Name: "fullName",
-						},
-						cli.StringFlag{
-							Name: "email",
-						},
-						cli.BoolFlag{
-							Name:  "admin",
-							Usage: "Indicates whether the user is a system admin",
-						},
-					},
-				},
-			},
-		},
-	}
-
-	app.Run(os.Args)
-}
+// This file is a place holder used to make the Godoc nicer to use.
