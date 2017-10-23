d5b635feef890f618f9ba949b6cb793cf6374802
Mathew Robinson <chasinglogic@gmail.com>
2017-10-23 12:12:16 -0400

    Make index page more useful

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -3,13 +3,45 @@
      can be found in the LICENSE file. -->
 
 <template>
-  <h1>Welcome to Praelatus!</h1>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h1>Welcome to Praelatus!</h1>
+        <p class="text-big">
+          Praelatus is an Open Source bug tracking and ticketing system.
+          If you're new to Praelatus and are wondering how to get started
+          you can view our <a href="https://doc.praelatus.io/">User Guide</a>.
+          Otherwise just sign in or sign up and get cracking!
+        </p>
+      </b-col>
+      <b-col>
+        <b-card no-body>
+          <b-tabs ref="tabs" card>
+            <b-tab title="Sign In" active>
+              <login></login>
+            </b-tab>
+            <b-tab title="Sign Up">
+              <login register="true"></login>
+            </b-tab>
+          </b-tabs>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+
 </template>
 
 
 <script>
+ import Login from '@/components/Users/Login'
+
  export default {
    name: 'index',
+
+   components: {
+     Login
+   },
+
    created: function () {
      if (this.$store.getters.currentUser !== {} &&
          this.$store.getters.currentUser !== null) {
@@ -18,3 +50,10 @@
    }
  }
 </script>
+
+
+<style>
+ .text-big {
+   font-size: 1.5rem;
+ }
+</style>
