fa157f195c6d45a80257e23127831d9dd0e5480c
Mathew Robinson <chasinglogic@gmail.com>
2017-10-24 06:48:46 -0400

    Clean up main.js add comments justifying dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -3,21 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import resource from 'vue-resource'
-import bootstrap from 'bootstrap-vue'
 import store from './store'
 
-import Axios from 'axios'
-// TODO: Set this during build time.
-Axios.defaults.baseURL = 'http://localhost:8080'
-
+import VueBootstrap from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(VueBootstrap)
 
+// HTTP Client we use.
+import Axios from 'axios'
+
+// TODO: Set these during build time.
+Axios.defaults.baseURL = 'http://localhost:8080'
 Vue.config.productionTip = false
-
-Vue.use(resource)
-Vue.use(bootstrap)
 
 /* eslint-disable no-new */
 new Vue({
