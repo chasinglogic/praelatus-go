ff3b94567ca046684120544663b5dc84ebb52b9c
Mathew Robinson <chasinglogic@gmail.com>
2017-10-25 12:25:25 -0400

    Fix bad syntax

--- client/src/components/General/Editor.vue
+++ client/src/components/General/Editor.vue
@@ -105,99 +105,98 @@
          return
        }
      }
-   },
 
-   wrapText: function (start, end, char) {
-     // Grab everything before the selection
-     this.text = this.text.slice(0, start) +
-                 // Grab the actual selection and wrap it in *
-                 char + this.text.slice(start, end) + char +
-                 // Grab the last of the text and concat each section to form
-                 // new text
-                 this.text.slice(end, this.text.length)
-   },
+     wrapText: function (start, end, char) {
+       // Grab everything before the selection
+       this.text = this.text.slice(0, start) +
+                   // Grab the actual selection and wrap it in *
+                   char + this.text.slice(start, end) + char +
+                   // Grab the last of the text and concat each section to form
+                   // new text
+                   this.text.slice(end, this.text.length)
+     },
 
-   unwrapText: function (start, end, char) {
-     let newStart = start - char.length
-     let newEnd = end + char.length + 1
-     let newSelectStart = start
-     let newSelectEnd = end + 1
+     unwrapText: function (start, end, char) {
+       let newStart = start - char.length
+       let newEnd = end + char.length + 1
+       let newSelectStart = start
+       let newSelectEnd = end + 1
 
-     if (this.text[start] === char[0]) {
-       newStart = newStart - 1
-       newSelectStart = newSelectStart + 1
+       if (this.text[start] === char[0]) {
+         newStart = newStart - 1
+         newSelectStart = newSelectStart + 1
+       }
+
+       if (this.text[end] === char[0]) {
+         newEnd = newEnd - 1
+         newSelectEnd = newSelectEnd - 1
+       }
+
+       let beg = this.text.slice(0, newStart)
+       let mid = this.text.slice(newSelectStart, newSelectEnd)
+       let rest = this.text.slice(newEnd, this.text.length)
+
+       this.text = beg + mid + rest
+     },
+
+     isSurroundedBy: function (start, end, char) {
+       return (this.text.slice(start - char.length, start) === char &&
+               this.text.slice(end, end + char.length) === char)
+     },
+
+     insertAt: function(x, text) {
+       // Grab the front of our string
+       let beg = this.text.slice(0, x) + text
+       // Grab everything after where we inserted.
+       let end = this.text.slice(x, this.text.length)
+       // Set the editor text equal to our newly eddited halves
+       this.text = beg + end
+     },
+
+     toggleItalic: function () {
+       let txta = document.getElementById(this.id)
+       let start = txta.selectionStart
+       let end = txta.selectionEnd
+       // Check if text is already wrapped in asterisks
+       if (this.isSurroundedBy(start, end, '*')) {
+         this.unwrapText(start, end, '*')
+       } else {
+         this.wrapText(start, end, '*')
+       }
+     },
+
+     toggleBold: function () {
+       let txta = document.getElementById(this.id)
+       let start = txta.selectionStart
+       let end = txta.selectionEnd
+       // Check if text is already wrapped in double asterisks
+       if (this.isSurroundedBy(start, end, '**')) {
+         this.unwrapText(start, end, '**')
+       } else {
+         this.wrapText(start, end, '**')
+       }
+     },
+
+     toggleCode: function () {
+       let txta = document.getElementById(this.id)
+       let start = txta.selectionStart
+       let end = txta.selectionEnd
+
+       console.log(this.text[start], this.text[start - 1])
+       console.log(this.text[end], this.text[end + 1])
+
+       if (this.isSurroundedBy(start, end, '```')) {
+         this.unwrapText(start, end, '```')
+       } else if (this.isSurroundedBy(start, end, '`')) {
+         this.unwrapText(start, end, '`')
+       } else if (this.text[start] === '\n' &&
+                  (this.text[end] === '\n' || this.text[end] === undefined)) {
+         this.wrapText(start, end, '```')
+       } else {
+         this.wrapText(start, end, '`')
+       }
      }
-
-     if (this.text[end] === char[0]) {
-       newEnd = newEnd - 1
-       newSelectEnd = newSelectEnd - 1
-     }
-
-     let beg = this.text.slice(0, newStart)
-     let mid = this.text.slice(newSelectStart, newSelectEnd)
-     let rest = this.text.slice(newEnd, this.text.length)
-
-     this.text = beg + mid + rest
    },
-
-   isSurroundedBy: function (start, end, char) {
-     return (this.text.slice(start - char.length, start) === char &&
-             this.text.slice(end, end + char.length) === char)
-   },
-
-   insertAt: function(x, text) {
-     // Grab the front of our string
-     let beg = this.text.slice(0, x) + text
-     // Grab everything after where we inserted.
-     let end = this.text.slice(x, this.text.length)
-     // Set the editor text equal to our newly eddited halves
-     this.text = beg + end
-   },
-
-   toggleItalic: function () {
-     let txta = document.getElementById(this.id)
-     let start = txta.selectionStart
-     let end = txta.selectionEnd
-     // Check if text is already wrapped in asterisks
-     if (this.isSurroundedBy(start, end, '*')) {
-       this.unwrapText(start, end, '*')
-     } else {
-       this.wrapText(start, end, '*')
-     }
-   },
-
-   toggleBold: function () {
-     let txta = document.getElementById(this.id)
-     let start = txta.selectionStart
-     let end = txta.selectionEnd
-     // Check if text is already wrapped in double asterisks
-     if (this.isSurroundedBy(start, end, '**')) {
-       this.unwrapText(start, end, '**')
-     } else {
-       this.wrapText(start, end, '**')
-     }
-   },
-
-   toggleCode: function () {
-     let txta = document.getElementById(this.id)
-     let start = txta.selectionStart
-     let end = txta.selectionEnd
-
-     console.log(this.text[start], this.text[start - 1])
-     console.log(this.text[end], this.text[end + 1])
-
-     if (this.isSurroundedBy(start, end, '```')) {
-       this.unwrapText(start, end, '```')
-     } else if (this.isSurroundedBy(start, end, '`')) {
-       this.unwrapText(start, end, '`')
-     } else if (this.text[start] === '\n' &&
-                (this.text[end] === '\n' || this.text[end] === undefined)) {
-       this.wrapText(start, end, '```')
-     } else {
-       this.wrapText(start, end, '`')
-     }
-   }
- },
 
    props: {
      startingText: ''
