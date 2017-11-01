c4c1cdf1e4098250d89f74ccbfd36353c0e98a3e
Mathew Robinson <chasinglogic@gmail.com>
2017-11-01 17:32:22 -0400

    Convert ql ast into a mongo query document

---
+++ repo/mongo/ast.go
@@ -0,0 +1,89 @@
+// Copyright 2017 Mathew Robinson <mrobinson@praelatus.io>. All rights reserved.
+// Use of this source code is governed by the AGPLv3 license that can be found in
+// the LICENSE file.
+
+package mongo
+
+import (
+	"github.com/praelatus/praelatus/ql/ast"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func eval(exp ast.InfixExpression) bson.M {
+	b := bson.M{}
+
+	switch exp.Operator {
+	case "AND":
+		opName := "$and"
+		if _, ok := b[opName]; ok {
+			b[opName] = append(b[opName].([]bson.M),
+				eval(exp.Left.(ast.InfixExpression)), eval(exp.Right.(ast.InfixExpression)))
+		} else {
+			b[opName] = []bson.M{
+				eval(exp.Left.(ast.InfixExpression)),
+				eval(exp.Right.(ast.InfixExpression)),
+			}
+		}
+	case "OR":
+		opName := "$or"
+		if _, ok := b[opName]; ok {
+			b[opName] = append(b[opName].([]bson.M),
+				eval(exp.Left.(ast.InfixExpression)), eval(exp.Right.(ast.InfixExpression)))
+		} else {
+			b[opName] = []bson.M{
+				eval(exp.Left.(ast.InfixExpression)),
+				eval(exp.Right.(ast.InfixExpression)),
+			}
+		}
+	case "=":
+		val := exp.Right.(ast.Literal)
+		makeFieldSearchDoc(exp, b, val.GetValue())
+	case "~":
+		val := exp.Right.(ast.Literal)
+		makeFieldSearchDoc(exp, b, bson.M{"$regex": val.GetValue()})
+	case "!=":
+		val := exp.Right.(ast.Literal)
+		makeFieldSearchDoc(exp, b, bson.M{"$ne": val.GetValue()})
+	case ">":
+		val := exp.Right.(ast.Literal)
+		makeFieldSearchDoc(exp, b, bson.M{"$gt": val.GetValue()})
+	case "<":
+		val := exp.Right.(ast.Literal)
+		makeFieldSearchDoc(exp, b, bson.M{"$lt": val.GetValue()})
+	case ">=":
+		val := exp.Right.(ast.Literal)
+		makeFieldSearchDoc(exp, b, bson.M{"$gte": val.GetValue()})
+	case "<=":
+		val := exp.Right.(ast.Literal)
+		makeFieldSearchDoc(exp, b, bson.M{"$lte": val.GetValue()})
+	}
+
+	return b
+}
+
+func evalAST(a ast.AST) bson.M {
+	infix, ok := a.Root.Expression.(ast.InfixExpression)
+	if !ok {
+		return nil
+	}
+
+	return eval(infix)
+}
+
+func makeFieldSearchDoc(exp ast.InfixExpression, b bson.M, valDoc interface{}) {
+	fn := exp.Left.(ast.FieldLiteral)
+	if fn.IsCustomField() {
+		customFieldDoc := bson.M{
+			"name":  fn.Value,
+			"value": valDoc,
+		}
+
+		if _, ok := b["fields"]; ok {
+			b["fields"] = append(b["fields"].([]bson.M), customFieldDoc)
+		} else {
+			b["fields"] = []bson.M{customFieldDoc}
+		}
+	} else {
+		b[fn.Value] = valDoc
+	}
+}
