1befb1d22e2965e488755ebb93c329b13291b271
Mathew Robinson <chasinglogic@gmail.com>
2017-11-05 17:28:30 -0500

    Make a notification model

---
+++ models/notification.go
@@ -0,0 +1,20 @@
+// Copyright 2017 Mathew Robinson <chasinglogic@gmail.com>. All rights reserved.
+// Use of this source code is governed by the AGPLv3 license that can be found in
+// the LICENSE file.
+
+package models
+
+import "gopkg.in/mgo.v2/bson"
+
+// Notification is any kind of activity on a project for which a user might want
+// an auditble history of
+type Notification struct {
+	ID             bson.ObjectId `bson:"_id" json:"id"`
+	Project        string        `json:"project"`
+	ActioningUser  string        `json:"actioningUser"`
+	ActionedTicket string        `json:"actionedTicket"`
+	Type           string        `json:"eventType"`
+	Body           string        `json:"body"`
+	Read           bool          `json:"read"`
+	Watcher        string        `json:"watcher"`
+}
