19e2311d067c1a5550040f5e8ab50b841f89b031
Mathew Robinson <chasinglogic@gmail.com>
2017-11-05 16:14:24 -0500

    Start cleanup of websocket code

--- events/sockets.go
+++ events/sockets.go
@@ -4,16 +4,51 @@
 
 package events
 
-import "github.com/gorilla/websocket"
+import (
+	"net/http"
 
-// WSManager wraps a websocket connection and should be managed inside of a
+	"github.com/gorilla/websocket"
+	"github.com/praelatus/praelatus/events/event"
+)
+
+// TODO: Add this to event system need to have a better design first
+
+var wsEventChan = make(chan event.Event, 10)
+
+// ManagedWebsocket wraps a websocket connection and should be managed inside of a
 // single go routine. Websockets are not safe for Concurrent reads and writes
-// and so the WSManager is used to guarantee that only one go routine is
+// and so the ManagedWebsocket is used to guarantee that only one go routine is
 // reading or writing at any given time.
-type WSManager struct {
-	In     chan []byte
-	Out    chan []byte
+type ManagedWebsocket struct {
 	Socket *websocket.Conn
-	InBuf  [1024]byte
-	OutBuf [1024]byte
+}
+
+// WSManager is used to send events to listening websockets
+type WSManager struct {
+	activeWS []ManagedWebsocket
+}
+
+var wsm = WSManager{
+	activeWS: make([]ManagedWebsocket, 0),
+}
+
+// AddWs will upgrade the given http connection to a websocket and register it
+// with the Websocket Manager
+func (e *WSManager) AddWs(w http.ResponseWriter, r *http.Request, h http.Header) error {
+	conn, err := websocket.Upgrade(w, r, h, 1024, 1024)
+	if err != nil {
+		return err
+	}
+
+	ws := ManagedWebsocket{
+		Socket: conn,
+	}
+
+	e.activeWS = append(e.activeWS, ws)
+	return nil
+}
+
+// AddWs calls the method of the same name on the global WSManager
+func AddWs(w http.ResponseWriter, r *http.Request, h http.Header) {
+	wsm.AddWs(w, r, h)
 }
