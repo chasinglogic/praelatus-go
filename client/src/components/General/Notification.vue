7643b5ae5634552d707c641bc33e841b2653a1e8
Mathew Robinson <chasinglogic@gmail.com>
2017-11-06 21:33:53 -0500

    Start on a notification vue component

---
+++ client/src/components/General/Notification.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="notification" v-html="renderedNotification">
+  </div>
+</template>
+
+<script>
+ import UserStub from '@/components/Users/Stub'
+
+ export default {
+   name: 'notification',
+
+   components: {
+     UserStub
+   },
+
+   computed: {
+     renderedNotification: function () {
+       let s = this.notification.body.split(' ')
+       let username = '<router-link :to=\'/users/' + s[0] + '\'>' +
+                      s[0] + '</router-link>'
+       let rest = s.slice(1)
+       return username + ' ' + rest.join(' ')
+     }
+   },
+
+   props: {
+     notification: {
+       type: Object,
+       required: true
+     }
+   }
+ }
+</script>
